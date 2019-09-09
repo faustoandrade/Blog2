99cc170601bf2a4cfb477d84c652d7cb81c32c84
faustoandrade <ayf1207@gmail.com>
2019-09-09 08:59:55 -0500

    added model, controller of comments and partials

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   # agregamos algunas validaciones
+  has_many :comments # ralación uno a muchos
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
 end
